805c56096555362bdcc7288a1b2805a9bec71de9
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-12-24 20:48:49 -0300

    Improved config UI

--- public/js/config.js
+++ public/js/config.js
@@ -2,10 +2,87 @@ let token, userId;
 
 const twitch = window.Twitch.ext;
 
+let main_item_count = 0;
+let extra_item_count = 0;
+
 var requests = { };
 
+let dom_info = {
+  "item": {"main": "main-item", "extra": "extra-item"},
+  "name": {"main": "main-name", "extra": "extra-name"},
+  "amount": {"main": "main-amount", "extra": "extra-amount"},
+  "list": {"main": "main-deck-list", "extra": "extra-deck-list"},
+  "count": {"main": 0, "extra": 0}
+}
+
+function update_card_items(which_deck){
+  for(let id = 0; id < dom_info["count"][which_deck]; id++){
+    let val_name = $("#" + dom_info["name"][which_deck] + id).val();
+    if(val_name.length == 0){
+      remove_card_item(id, which_deck);
+      id = 0;
+    }
+  }
+
+  add_card_item(which_deck);
+}
+
+function add_card_item(which_deck){
+  for(let id = 0; id < dom_info["count"][which_deck]; id++){
+    let val_name = $("#" + dom_info["name"][which_deck] + id).val();
+    if(val_name.length == 0) return;
+  }
+
+  let html = '<div class="row" id="' + dom_info["item"][which_deck] + dom_info["count"][which_deck] + '"><div class="three columns"><input class="u-full-width" type="number" min="1" max="3" value="3" id="' + dom_info["amount"][which_deck] + dom_info["count"][which_deck] + '"></div><div class="nine columns"><input class="u-full-width" type="text" placeholder="Enemy Controller" id="' + dom_info["name"][which_deck] + dom_info["count"][which_deck] + '"></div></div>';
+
+  dom_info["count"][which_deck]++;
+
+  $(html).focusout(function(){
+    update_card_items(which_deck);
+  }).appendTo("#" + dom_info["list"][which_deck]);
+}
+
+function remove_card_item(index, which_deck){
+  $("#" + dom_info["item"][which_deck] + index).remove();
+
+  for(let id = index; id < dom_info["count"][which_deck]; id++){
+    $("#" + dom_info["item"][which_deck] + id).attr("id", dom_info["item"][which_deck] + (id - 1));
+    $("#" + dom_info["amount"][which_deck] + id).attr("id", dom_info["amount"][which_deck] + (id - 1));
+    $("#" + dom_info["name"][which_deck] + id).attr("id", dom_info["name"][which_deck] + (id - 1));
+  }
+
+  dom_info["count"][which_deck]--;
+}
+
+function clear_card_items(which_deck){
+  for(let id = 0; id < dom_info["count"][which_deck]; id++){
+    $("#" + dom_info["item"][which_deck] + id).remove();
+  }
+
+  dom_info["count"][which_deck] = 0;
+  add_card_item(which_deck);
+}
+
 function update_requests(){
-  let deck = Deck.from_plain_text($("#deck-text").val());
+  let deck = new Deck();
+  for(let id = 0; id < dom_info["count"]["main"]; id++){
+    if($("#" + dom_info["name"]["main"] + id).val().length > 0){
+      deck.main.push({
+        "name": $("#" + dom_info["name"]["main"] + id).val(),
+        "amount": parseInt($("#" + dom_info["amount"]["main"] + id).val()),
+      });
+    }
+  }
+
+  for(let id = 0; id < dom_info["count"]["extra"]; id++){
+    if($("#" + dom_info["name"]["extra"] + id).val().length > 0){
+      deck.extra.push({
+        "name": $("#" + dom_info["name"]["extra"] + id).val(),
+        "amount": parseInt($("#" + dom_info["amount"]["extra"] + id).val()),
+      });
+    }
+  }
+
   let encoded_deck = '?deck=' + encodeURI(deck.to_json());
 
   requests = {
@@ -18,7 +95,22 @@ function update_requests(){
 
 function update_deck_text(deck_json) {
   let deck = Deck.from_json(deck_json);
-  $('#deck-text').val(deck.to_plain_text());
+
+  for(let i = 0; i < deck.main.length; i++) add_card_item("main");
+  for(let i = 0; i < deck.extra.length; i++) add_card_item("extra");
+
+  for(let i = 0; i < deck.main.length; i++){
+    $("#" + dom_info["name"]["main"] + i).val(deck.main[i].name);
+    $("#" + dom_info["amount"]["main"] + i).val(deck.main[i].amount);
+  }
+
+  for(let i = 0; i < deck.extra.length; i++){
+    $("#" + dom_info["name"]["extra"] + i).val(deck.extra[i].name);
+    $("#" + dom_info["amount"]["extra"] + i).val(deck.extra[i].amount);
+  }
+
+  update_card_items("main");
+  update_card_items("extra");
 }
 
 twitch.onContext((context) => { });
@@ -34,13 +126,21 @@ twitch.onAuthorized((auth) => {
 });
 
 $(function(){
+  add_card_item("main");
+  add_card_item("extra");
+
+  $("#clear-deck").click(function(){
+    clear_card_items("main");
+    clear_card_items("extra");
+  });
+
   $("#set-deck").click(function(){
     if(!token) return twitch.rig.log('Not authorized');
     update_requests();
     $.ajax(requests.set);
   });
 
-  twitch.listen('broadcast', function (target, contentType, deck) {
+  twitch.listen('broadcast', function (target, content_type, deck) {
     update_deck_text(deck);
   });
 });
