a42c88339aa22b25859bfe2af610d9f8f0b0b127
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-12-24 23:23:52 -0300

    Moved to server config

--- public/js/general.js
+++ public/js/general.js
@@ -5,24 +5,24 @@ function set_auth(token) {
 }
 
 // ----------- HEROKU ENVIRONMENT -----------
-// function create_request(type, method, options, success) {
-//   return {
-//     type: type,
-//     url: 'https://duel-links-twitch-extension.herokuapp.com/deck/' + method + options,
-//     success: success,
-//     error: log_error
-//   }
-// }
-
-// ----------- LOCAL ENVIRONMENT -----------
 function create_request(type, method, options, success) {
   return {
     type: type,
-    url: location.protocol + '//localhost:8081/deck/' + method + options,
+    url: 'https://duel-links-twitch-extension.herokuapp.com/deck/' + method + options,
     success: success,
     error: log_error
   }
 }
+
+// ----------- LOCAL ENVIRONMENT -----------
+// function create_request(type, method, options, success) {
+//   return {
+//     type: type,
+//     url: location.protocol + '//localhost:8081/deck/' + method + options,
+//     success: success,
+//     error: log_error
+//   }
+// }
 
 function log_error(_, error, status) {
   twitch.rig.log('EBS request returned ' + status + ' (' + error + ')');
